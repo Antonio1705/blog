fb1261d19948704c5ccaab21dbb03ef631eb9d86
Antonio1705 <antoniododevski98@gmail.com>
2024-07-24 15:50:10 +0200

    config

--- blog/src/main/java/de/example/blog/repository/CommentRepository.java
+++ blog/src/main/java/de/example/blog/repository/CommentRepository.java
@@ -3,7 +3,9 @@ package de.example.blog.repository;
 import de.example.blog.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
